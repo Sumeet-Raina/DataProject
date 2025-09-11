61d14ad90b219e4849fcad3018af53e32f41ce7c
Ridwan Kawsar <ridwan_kawsar@hotmail.co.uk>
2025-09-11 12:06:37 +0100

    Added logging and validation

--- Employees/src/main/java/org/readwriteemployees/App.java
+++ Employees/src/main/java/org/readwriteemployees/App.java
@@ -3,18 +3,42 @@ package org.readwriteemployees;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.stream.Collectors;
 
 public class App {
-//  public static void main(String[] args) {
-//    //String path = "src/main/resources/employees_short.csv";
-//
-//
-//    // Method 1 - createEmployee Obj
-//    String line = "629539,Dr.,Del,I,Fernandez,M,del.fernandez@hotmail.com,8/13/1991,4/7/2016,138662";
-////    Employee e = EmployeeCsvReader.createEmployee(line);
-//    System.out.println(e);
-//
-//  }
+    public static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            logger.setLevel(Level.ALL);
+            FileHandler fileHandler = new FileHandler("app.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+
+            logger.info("Application started.");
+
+            String path = "src/main/resources/employees.csv"; // full test file
+            EmployeeCsvReader reader = new EmployeeCsvReader();
+            ArrayList<Employee> employees = reader.readEmployees(path);
+
+            logger.log(Level.INFO, "Loaded {0} employees successfully.", employees.size());
+
+            ArrayList<String> faulty = reader.getFaultyLines();
+            logger.log(Level.WARNING, "Found {0} faulty lines.", faulty.size());
+
+            for (String badLine : faulty) {
+                logger.warning("Faulty: " + badLine);
+            }
+
+            logger.info("Application finished.");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Application error", e);
+        }
+    }
 }
